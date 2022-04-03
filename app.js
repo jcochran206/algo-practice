90811751424b741bb6fa04b8d9e4727f2053b174
jcochran206 <jonathan.o.cochran@gmail.com>
2022-04-03 12:45:47 -0700

    added additional functions

--- app.js
+++ app.js
@@ -75,3 +75,95 @@ function factorial(num){
     }
 }
 factorial(13);
+
+/*
+Write a function that is given an array and each time the value is "food" it should console log "yummy". 
+If "food" was not present in the array console log "I'm hungry" once.
+*/
+
+function alwaysHungry(arr) {
+    let foodCount = 0;
+    for(let i = 0; i <= arr.length; i++){
+        if(arr[i] == "food"){
+            console.log("yummy");
+            foodCount++;
+        }
+    }
+    if(foodCount == 0){
+        console.log("I am hungry")
+    }
+}
+   
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
+
+/*
+Given an array and a value cutoff, return a new array 
+containing only the values larger than cutoff.
+*/
+
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for(let i = 0; i <= arr.length; i++){
+        if(arr[i] > cutoff){
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
+/* 
+Given an array of numbers return a count of 
+how many of the numbers are larger than the average.
+*/
+function betterThanAverage(arr) {
+    var sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]; 
+    }
+    var avg = sum / arr.length;
+    var count = 0
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i] > avg){
+            count++;
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
+
+//write a function that will reverse the values an array and return them
+function rev(arr){
+    let left = 0; 
+    let right = arr.length - 1; 
+    while(left<right){
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+    return arr;
+}
+var result = rev(["a", "b", "c", "d", "e"]);
+console.log(result);
+
+//write a function that will return fibonacci numbers up to given length;
+
+function fib(n){
+    let fibArr = [0,1];
+    while(fibArr.length < n){
+        let prev = fibArr[fibArr.length - 1];
+        let last = fibArr[fibArr.length-2];
+        fibArr.push(prev + last);
+    }
+    return fibArr
+}
+
+let result = fib(10);
+console.log(result); 
