88d244ffb9f6957cc8e374761f51c62e4faff514
jcochran206 <jonathan.o.cochran@gmail.com>
2022-04-12 14:54:56 -0700

    refactored methods in user to allow for chaining

--- user.py
+++ user.py
@@ -14,21 +14,25 @@ class User:
     #method to display user balance
     def display_user_balance(self):
         print(f'{self.name} balance is: {self.account_balance}')
+        return self #needed to chain method
     #method to deposit to account
     def deposit(self, amount):
         #add money to account
         self.account_balance += amount
         print(f'{self.name} deposited: {amount}')
+        return self #need to chain method
     #method to withdraw amount
     def make_withdrawal(self, amount): 
         self.account_balance -= amount
         print(f'{self.name} withdrew : {amount}')
+        return self #need to chain
     #method to transfer
     def tranfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
         print(f'{self.name} transfered {amount} to {other_user.name}')
         print(f'{self.name} new balance is: {self.account_balance}')
+        return self #needed to chain
 
 John = User("John", "john@email.com")
 Todd = User("Todd", "Todd@email.com")
@@ -39,27 +43,15 @@ print(Todd.name)
 print(Sarah.name)
 print('-----------')
 #1st user to make 3 deposits
-print('-----1st user------')
-John.deposit(500)
-John.deposit(200)
-John.deposit(300)
-John.make_withdrawal(50)
-John.display_user_balance()
+print('-----1st user------') #refactored for chain methods
+John.deposit(500).deposit(200).deposit(300).make_withdrawal(50).display_user_balance()
 print('----------------')
 #2nd user to make 3 deposits
-print('-----2nd user------')
-Todd.deposit(300)
-Todd.deposit(200)
-Todd.make_withdrawal(10)
-Todd.make_withdrawal(50)
-Todd.display_user_balance()
+print('-----2nd user------') #refactored for chain methods
+Todd.deposit(300).deposit(200).make_withdrawal(10).make_withdrawal(50).display_user_balance()
 print('----------------')
 #2nd user to make 3 deposits
-print('-----3rd user------')
-Sarah.deposit(1000)
-Sarah.make_withdrawal(300)
-Sarah.make_withdrawal(50)
-Sarah.make_withdrawal(80)
-Sarah.display_user_balance()
+print('-----3rd user------') #refactored for chain methods
+Sarah.deposit(1000).make_withdrawal(300).make_withdrawal(50).make_withdrawal(80).display_user_balance()
 print('----------------')
 John.tranfer_money(Todd,200)
