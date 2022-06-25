dc4f6354fb0d784b546a996fbab606883bba136c
jcochran206 <jonathan.o.cochran@gmail.com>
2022-06-25 10:09:31 -0700

    created function greater than y

--- AlgosinJS.js
+++ AlgosinJS.js
@@ -58,14 +58,43 @@ oddArr();
 
 //iterate and print array
 //iterate thru a given array, printing each value
+function printArr(){
+    let arr = [2,3,5,8,9]
+    for(let i =0; i < arr.length; i++){
+        console.log(arr[i])
+    }
+}
+printArr();
 
 //Get and Print average
 //analyze an array's values and print the average
-
+function getAvg(){
+    let arr = [1,4,6,9];
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i] 
+    }
+    let avg = sum / arr.length
+    console.log('get average' , avg); 
+}
+getAvg(); //output is 4
 
 //Greater than Y
 //given an array and a value Y, count and print the number of array values greater than Y
+let greaterArr = [2,8,12,14];
 
+function greaterThanY(){
+    let count = 0;
+    let y = 10;
+    for(let i = 0; i <= greaterArr.length; i++){
+        if(greaterArr[i] > 10){
+            count++
+        }
+    }
+    console.log('number of values greater than y : ',count)
+    
+}
+greaterThanY()
 
 //Min Max Avg
 //given an arr print the max min and average of the array
