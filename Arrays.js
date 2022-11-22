96f5d82d2f02dcf1459e7817512178e5befa40b1
jcochran206 <jonathan.o.cochran@gmail.com>
2022-11-21 23:11:47 -0800

    added container problem

--- Arrays.js
+++ Arrays.js
@@ -11,6 +11,29 @@ function findTwoSum(arr,tgt){
             }
         }
     }
+    return null
 }
 findTwoSum(arr,tgt);
 
+/* given an array of positive integers where each integer represents
+the height of a vertical line on a chart. Find two lines whic together with the x-axis
+forms a container that would hold the greatest amount of water. Return the area of water it holds
+hint: area = l x w
+*/
+
+let heightsArray = [7,1,2,3,9]; 
+function getMaxWaterContainer(heights){
+    let maxArea = 0;
+    for(let p1 = 0; p1 < heights.length; p1++){
+        for(let p2 = p1 + 1; p2 < heights.length; p2++){
+            const height = Math.min(heights[p1], heights[p2]);
+            const width = p2 - p1;
+            const area = height * width;
+
+            maxArea = Math.max(maxArea, area);
+        }
+    }
+    
+    return maxArea;
+}
+console.log(getMaxWaterContainer(heightsArray), 'expected 28');
