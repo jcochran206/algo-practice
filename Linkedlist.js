23c83e6a8453cef4f1fea32233357871960bf2a3
jcochran206 <jonathan.o.cochran@gmail.com>
2022-12-24 23:37:29 -0800

    modified linked list for multi-layer

--- Linkedlist.js
+++ Linkedlist.js
@@ -38,3 +38,95 @@ that can point to a separate doubly linked list
 these child lists can also have one or more child dbl linked lists of their 
 own and so on
 */
+class ListNode2 {
+    constructor(val, next = null, prev = null, child = null) {
+      this.val = val;
+      this.next = next;
+      this.prev = prev;
+      this.child = child;
+    }
+}
+
+const nodes = [1, [2, 7, [8, 10, 11], 9], 3, 4, [5, 12, 13], 6]
+
+const buildMultiLevel = function(nodes) {
+  const head = new ListNode2(nodes[0]);
+  let currentNode = head;
+
+  for(let i = 1; i < nodes.length; i++) {
+    const val = nodes[i];
+    let nextNode;
+
+    if(Array.isArray(val)) {
+      nextNode = buildMultiLevel(val);
+      currentNode.child = nextNode;
+      continue;
+    }
+
+    nextNode = new ListNode(val);
+    currentNode.next = nextNode;
+    nextNode.prev = currentNode;
+    currentNode = nextNode;
+  }
+  
+  return head;
+}
+
+let multiLinkedList = buildMultiLevel(nodes);
+
+// ---- Generate our linked list ----
+
+const printListMulti = head => {
+  if(!head) {
+    return;
+  }
+
+  console.log(head.val)
+
+  if(head.child) {
+    printListMulti(head.child);
+  }
+
+  printListMulti(head.next);
+}
+
+const printList2 = head => {
+    if(!head) {
+      return;
+    }
+  
+    console.log(head.val);
+    
+    printList(head.next);
+}
+
+const flatten = function (head) {
+    if (!head) return head;
+  
+    let currentNode = head;
+    while (currentNode !== null) {
+      if (currentNode.child === null) {
+        currentNode = currentNode.next;
+      } else {
+        let tail = currentNode.child;
+        while(tail.next !== null) {
+          tail = tail.next;
+        }
+  
+        tail.next = currentNode.next;
+        if (tail.next !== null) {
+          tail.next.prev = tail;
+        }
+  
+        currentNode.next = currentNode.child;
+        currentNode.next.prev = currentNode;
+        currentNode.child = null;
+    }
+}
+  
+    return head;
+};
+  
+  printListMulti(multiLinkedList);
+  console.log('after flatten');
+  printList2(flatten(multiLinkedList));
